0853e7564d2a375588753495435297e0b43ebc62
Steve Fuller <wsfuller.dev@gmail.com>
2025-11-15 14:50:09 -0800

    sign in and up AuthActions component for AppBar

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,17 +1,22 @@
+'use client';
 import Image from 'next/image';
 
 import { Flex, Title } from '@mantine/core';
 
+import useSession from '@/lib/hooks/useSession';
 import UserMenu from './UserMenu';
+import AuthActions from './AuthActions';
 
 export default function AppBar() {
+  const { session } = useSession();
+
   return (
     <Flex h="100%" align="center" justify="space-between" p="md">
       <Flex align="center">
         <Image src="/toliet.svg" alt="Logo" width={45} height={45} />
         <Title order={4}>Opt Wide Outside FF</Title>
       </Flex>
-      <UserMenu />
+      {session ? <UserMenu /> : <AuthActions />}
     </Flex>
   );
 }
